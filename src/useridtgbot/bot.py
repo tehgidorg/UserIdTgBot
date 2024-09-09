12dc98c55b517192f53005025bfd85d699d5fa1e
vadyushkins <vadim.i.abzalov@gmail.com>
2024-09-10 00:18:09 +0300

    Add buttons

--- src/useridtgbot/bot.py
+++ src/useridtgbot/bot.py
@@ -1,11 +1,23 @@
-from telegram import Update
-from telegram.ext import (ApplicationBuilder, CallbackContext, CommandHandler,
-                          MessageHandler, filters)
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import (
+    ApplicationBuilder,
+    CallbackContext,
+    CommandHandler,
+    MessageHandler,
+    filters,
+    CallbackQueryHandler,
+)
 
 
 async def start_handler(update: Update, context: CallbackContext) -> None:
+    keyboard = [
+        [InlineKeyboardButton("Получить свой ID", callback_data="id")],
+        [InlineKeyboardButton("Получить свой ник", callback_data="username")],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
     await update.message.reply_text(
-        "Привет! Я простой Telegram-бот. Чтобы узнать, что я могу, напиши /help."
+        "Привет! Я простой Telegram-бот. Выберите действие:", reply_markup=reply_markup
     )
 
 
@@ -14,24 +26,33 @@ async def help_handler(update: Update, context: CallbackContext) -> None:
         "Доступные команды:\n"
         "/start - запустить бота\n"
         "/help - получить доступные команды\n"
-        "/id - получить свой ID\n"
-        "/username - получить свой ник"
     )
     await update.message.reply_text(response)
 
 
 async def id_handler(update: Update, context: CallbackContext) -> None:
     response = f"Ваш ID: {update.effective_user.id}"
-    await update.message.reply_text(response)
+    await update.callback_query.message.reply_text(response)
 
 
 async def username_handler(update: Update, context: CallbackContext) -> None:
-    response = f"Ваш ник: {update.message.chat.username}"
-    await update.message.reply_text(response)
+    response = f"Ваш ник: {update.effective_user.username}"
+    await update.callback_query.message.reply_text(response)
 
 
 async def message_handler(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text(f"Вы написали: {update.message.text}")
+
+
+async def button_handler(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+
+    if query.data == "id":
+        await id_handler(update, context)
+    elif query.data == "username":
+        await username_handler(update, context)
+
+    await query.answer()
 
 
 def main():
@@ -41,9 +62,8 @@ def main():
 
     app.add_handler(CommandHandler("start", start_handler))
     app.add_handler(CommandHandler("help", help_handler))
-    app.add_handler(CommandHandler("id", id_handler))
-    app.add_handler(CommandHandler("username", username_handler))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
+    app.add_handler(CallbackQueryHandler(button_handler))
 
     app.run_polling()
 
